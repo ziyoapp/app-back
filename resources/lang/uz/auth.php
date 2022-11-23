6a48ab73fb9e71f4a39f4dc73656410eb6084ea4
Dilshod Muydinov <uktamovich95@gmail.com>
2022-11-23 18:36:10 +0500

    validation messages translate

---
+++ resources/lang/uz/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Authentication Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used during authentication for various
+| messages that we need to display to the user. You are free to modify
+| these language lines according to your application's requirements.
+|
+*/
+
+return [
+    'failed'   => 'Bunday maʼlumotlarga ega foydalanuvchi mavjud emas.',
+    'password' => 'Taqdim etilgan parol noto‘g‘ri.',
+    'throttle' => 'Kirish uchun juda ko‘p urinishlar aniqlandi. Iltimos :seconds soniyadan so‘ng yana urinib ko‘ring.',
+];
